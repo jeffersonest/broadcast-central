bd86599e05f2f3dd0f3f9b4403ac20ca9afdf2e4
allcj <allcj@cin.ufpe.br>
2017-08-28 17:45:11 -0300

    só saio hoje quando esse app funcionar

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var server = restify.createServer();
 var io = require('socket.io')(server.server);
 server.use(restify.bodyParser());
 
-server.get('/ping/:name', function(req,res){
+/*server.get('/ping/:name', function(req,res){
     io.emit('ping', req.params.name);
     console.log('Recebendo Ping');
     res.send(200);
@@ -16,7 +16,19 @@ server.post('/geo', function(req,res){
     });
     console.log(req.body);
     res.send(200);
+});*/
+
+io.on('connection',(socket)=>{
+    console.log(socket.id);
 });
+
+server.post('/notify', function(req, res) {
+    var emergency = req.body;
+    console.log(emergency);
+    io.emit('new_emergency',emergency);
+    res.send(200);
+});
+
 server.listen(9000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
